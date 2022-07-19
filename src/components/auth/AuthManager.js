3815c711f91c26759568568834e6c45d6c7caa6b
Lauden Eller <laudlauden@gmail.com>
2022-07-19 10:03:21 -0800

    Changed LoginUser function

--- src/components/auth/AuthManager.js
+++ src/components/auth/AuthManager.js
@@ -6,13 +6,17 @@ export const registerUser = (user) => {
 }
 
 export const loginUser = (user) => {
-  return fetch("http://127.0.0.1:8000/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify(user)
-  })
-    .then(res => res.json())
+  return fetchIt(`${Settings.API}/login`, 'POST', JSON.stringify(user))
 }
+
+// export const loginUser = (user) => {
+//   return fetch("http://127.0.0.1:8000/login", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(user)
+//   })
+//     .then(res => res.json())
+// }
